4b44b888fae96cb7bdabd310440f0ab295d8d6df
Zach Whitehead <zachmanw@gmail.com>
2019-08-01 23:33:24 -0400

    Add initial OpenPPG controller config

---
+++ boards/openppg_c2/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "OpenPPG"
+#define PRODUCT_NAME "Controller C2"
+#define VOLUME_LABEL "OPENPPGC2"
+#define INDEX_URL "https://openppg.com/"
+#define BOARD_ID "SAMD21G18A-OpenPPG-c2"
+
+#define USB_VID 0x239A
+#define USB_PID 0x0015
+
+#define LED_PIN PIN_PA07
+
+#endif
